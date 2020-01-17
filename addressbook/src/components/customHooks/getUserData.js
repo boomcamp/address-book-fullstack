37ed6979bd762d83e3aa7be8fbee1c0b2349c633
josephazores <joseph.azores@gmail.com>
2020-01-17 14:20:56 +0800

    final

--- addressbook/src/components/customHooks/getUserData.js
+++ addressbook/src/components/customHooks/getUserData.js
@@ -3,12 +3,9 @@ import Axios from "axios";
 
 export const getUserData = async user => {
   try {
-    const response = await Axios.get(
-      `${url}/user/${user.id}/addressbook?sort=asc`,
-      {
-        headers: { Authorization: `Bearer ${user.token}` }
-      }
-    );
+    const response = await Axios.get(`${url}/user/${user.id}/addressbook`, {
+      headers: { Authorization: `Bearer ${user.token}` }
+    });
     return response.data;
   } catch (err) {
     console.log(err);
